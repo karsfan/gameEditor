d8e0c136e70b6a2aad48a93de3a3a7fe341203ef
karsfan <karsfan3001@gmail.com>
2017-06-28 17:11:29 +0200

    Aiuto

--- src/com/levels/editor/ToolsPanel.java
+++ src/com/levels/editor/ToolsPanel.java
@@ -6,11 +6,6 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
